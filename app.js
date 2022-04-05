3e2745bb36355816f98636e458b30790ca2ec037
sachinyadav <sachinyadav@cedcoss.com>
2022-04-05 10:44:18 +0530

    search button function

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 
-
-var table = `<table>
-<tr>
-    <th>Company</th>
-    <th>Model</th>
-    <th>Memory</th>
-    <th>Price</th>
-</tr>`
-var tfooter = "</table>"  
-
+var companyinfo =[];
 function add(){
-    var companyinfo =[];
+    
     var company =document.getElementById("company").value;
     var model =document.getElementById("model").value;
     var memory =document.getElementById("memory").value;
@@ -23,15 +14,51 @@ function add(){
         "Rs": `${Rs}`,
     };
     companyinfo.push(data);
+    display();
+};
+  var table = `<table>
+        <tr>
+            <th>Company</th>
+            <th>Model</th>
+            <th>Memory</th>
+            <th>Price</th>
+        </tr>`
+    var tfooter = "</table>"
 
+function display(){
+    var row = ""
     companyinfo.forEach(element => {
-        table +=`  <tr>
+        row +=`  <tr>
                 <td>${element.Name}</td>
                 <td>${element.Model}</td>
                 <td>${element.Memory}</td>
                 <td>${element.Rs}</td>
             </tr>`
     });        
-    document.getElementById("table").innerHTML=table+tfooter;
+    document.getElementById("table").innerHTML=table+row+tfooter;
+};
 
+function search() {
+    var select = document.getElementById("selectfield").value;
+    console.log(select);
+    var item = document.getElementById("item").value;
+    console.log(item);
+    var row = "";
+    companyinfo.forEach(element => {
+        console.log(element["Name"])
+         if (element[`${select}`] == item){
+                row +=`  <tr>
+                    <td>${element.Name}</td>
+                    <td>${element.Model}</td>
+                    <td>${element.Memory}</td>
+                    <td>${element.Rs}</td>
+                </tr>`
+            }
+            else{
+                console.log("not valid")
+                
+                display();
+            }
+    });
+    document.getElementById("table").innerHTML=table+row+tfooter;  
 }
